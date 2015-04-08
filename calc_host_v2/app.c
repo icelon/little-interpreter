6a3e53550f2dadccce9dc218b2981b24de21c656
legacy <carlojpisani@gmail.com>
2015-04-09 01:06:37 +0200

    calc v2

--- calc_host_v2/app.c
+++ calc_host_v2/app.c
@@ -2,7 +2,7 @@
 
 char_t msg0[] = "[*] mycalc v2.1 24/04/2002";
 char_t msg1[] = "       restyled 24/06/2014";
-char_t msg2[] = "    iset={+,-,*,/,!,=,?,<,>,a..z,A..Z,0..9}";
+char_t msg2[] = "    iset={+,-,*,/,!,=,?,<,>,a..z,A..F,0..9}";
 
 void app()
 {
